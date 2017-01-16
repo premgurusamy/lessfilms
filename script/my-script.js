57cdfaf08dbcba92f3a02ec50357f0857b6ce075
Prem Gurusamy <herepremg@gmail.com>
2017-01-16 16:51:10 +0530

    navbar updated

--- script/my-script.js
+++ script/my-script.js
@@ -7,3 +7,20 @@
  $('.dropdown').on('hide.bs.dropdown', function() {
    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
  });
+
+
+ $(document).ready(function () {
+     function CloseNav() {
+         $(".navbar-collapse").stop().css({ 'height': '1px' }).removeClass('in').addClass("collapse");
+         $(".navbar-toggle").stop().removeClass('collapsed');
+     }
+
+     $('html').click(function (event) {
+         var clickover = $(event.target);
+         var _opened = $(".navbar-collapse").hasClass("navbar-collapse in");
+         if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+             CloseNav();
+         }
+
+     });
+ });
